5755d575630be561d11b60087f62fe731b5c12e5
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-01-26 09:23:57 +0100

    a

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     x = number % 10 if number > 0 else number % -10
-    print("{:d}".format(x), end="")
+    print("{:d}".format(abs(x)), end="")
     return x
