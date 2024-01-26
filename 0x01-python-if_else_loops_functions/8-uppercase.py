634690cceab7938882f1c0d6284b9dd7cc1021ea
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-01-26 09:00:01 +0100

    a

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122 or ord(str[i]):
+            print("{:c}".format(ord(str[i]) - 32), end="" if ord(str[i]) != 32 else " ")
