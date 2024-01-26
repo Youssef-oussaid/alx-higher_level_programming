d3433a821a9eab183e8ca2df976d319427e9abde
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-01-27 00:07:44 +0100

    a

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,3 +1,5 @@
+#include <bits/c++config.h>
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -15,7 +17,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
+size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
