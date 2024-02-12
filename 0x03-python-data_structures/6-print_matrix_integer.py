b2163bbd7f4c6e690d015745ff46f820f11ca549
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-12 22:01:00 +0100

    a

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for submatrix in len(matrix):
+    for submatrix in matrix:
         if len(submatrix) == 0:
             print()
         for i in range(len(submatrix)):
