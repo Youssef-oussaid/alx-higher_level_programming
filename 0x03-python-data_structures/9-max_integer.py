caba151aa6087775092d2192e6a0d315dff5026a
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-13 06:09:47 +0100

    a

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
-    x = my_list[0]
-    for i in my_list:
-        x = my_list[i] if my_list[i] > x else x
-    return x
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
+
