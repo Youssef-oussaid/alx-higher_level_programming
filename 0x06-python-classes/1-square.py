26cbc35f31e8e2f6a1d99f433e6e917433b4274a
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-21 08:57:41 -0800

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Module."""
+
+
+class Square:
+    """Square class."""
+    def __init__(self, size):
+        """a function to initialize the instances"""
+        self._size = size
