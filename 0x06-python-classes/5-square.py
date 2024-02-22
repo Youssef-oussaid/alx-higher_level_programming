23f7c4bcfe22a05b28fe5b9f44de43d52c0f3a1f
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-22 06:00:11 +0100

    a

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Square Module."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size=0):
+        """constructor
+
+        Args
+            size: Length of a side of a square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """property for the length of a side of a square
+
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Area of this square
+        Returns:
+            the size of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints this Square
+        """
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size -1 and i != j else "")
+        print()
