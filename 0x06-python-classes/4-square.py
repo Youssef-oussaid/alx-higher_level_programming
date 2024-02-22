b1cab09dfef43aab0ee82f61f1e3fc5bc7c67a4f
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-22 05:51:45 +0100

    a

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,7 +22,7 @@ class Square:
             ValueError: if less than 0
         """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -30,7 +30,7 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
-    
+
     def area(self):
         """Area of this square
         Returns:
