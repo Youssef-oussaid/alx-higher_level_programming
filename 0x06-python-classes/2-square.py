a8d3a169bad1f2be0f990764062de70412b45076
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-21 20:31:26 -0800

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Square Module"""
+"""Square Module."""
 
 
 class Square:
-    """Define a square"""
+    """Define a square."""
 
-    def __init__(self, size=0)
+    def __init__(self, size=0):
         """constructor
 
         Args
