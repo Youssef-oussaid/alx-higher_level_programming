ae6488243eb2700ae173924205104418535e5618
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-23 20:29:05 +0100

    a

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,22 +3,22 @@
 
 
 class Square:
-    """Represent a square"""
-    
+    """Respresent a square"""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square
-        Args: 
-            size (int): The size of the square
+        Args:
+            size (int): The qize of the square
             position (int, int): The position of the new square
         """
         self.size = size
         self.position = position
-        
+
     @property
     def size(self):
-        """Get the current size of the square"""
+        """Get/set the current size of the square"""
         return self.__size
-        
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,17 +26,19 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
-        """Get the current position of the square"""
+        """Get/set the current position of the square"""
         return self.__position
-    
+
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) for num in value)
+            or not all(num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
