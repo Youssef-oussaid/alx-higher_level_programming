071808d56ae1e2cb16c727b2fbd609774563f63b
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-23 22:30:32 +0100

    a

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -17,7 +17,7 @@ class Square:
     @property
     def size(self):
         """Get/set the current size of the square"""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -30,20 +30,22 @@ class Square:
     @property
     def position(self):
         """Get/set the current position of the square"""
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) for num in value)
+            or not all(num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
         """Return the current area of the square"""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
 
     def my_print(self):
         """Prints the square with the # character"""
@@ -56,10 +58,9 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-            
+
     def __str__(self):
-        """_summary_
-        """
+        """_summary_"""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
@@ -67,4 +68,4 @@ class Square:
             [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
-        return ("")
+        return ""
