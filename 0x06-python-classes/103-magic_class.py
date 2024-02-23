44db8b0e4b8fff742a0c9252e87b5362bdb4bf31
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-02-23 22:48:25 +0100

    a

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,8 +9,8 @@ from networkx import radius
 
 class MagicClass:
     """A circle"""
-    
-    def __init__(self):
+
+    def __init__(self, radius=0):
         """Initialize a MagicClass
         Args:
             radius (float or int): The radius of a circle
@@ -19,11 +19,11 @@ class MagicClass:
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be an integer")
         self.__radius = radius
-        
+
     def area(self):
         """Return the area"""
-        return (self.__radius ** 2 * math.pi)
-    
+        return self.__radius**2 * math.pi
+
     def circumference(self):
         """Return the circumference"""
-        return (2 * math.pi * self.__radius)
+        return 2 * math.pi * self.__radius
