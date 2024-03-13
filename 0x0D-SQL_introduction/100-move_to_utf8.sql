d9629a0ea398a2f4ee386fd6069250ec248c0c2b
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-13 12:23:24 +0000

    new commit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 USE hbtn_0c_0;
-
--- Convert the table's default character set and collation
-ALTER TABLE first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
--- Convert the field's character set and collation
-ALTER TABLE first_table
-    MODIFY COLUMN name VARCHAR(255)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
