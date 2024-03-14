66ec7e9b84a270fe76b2ce19aaa60476ace54fb2
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-14 12:50:09 +0000

    new commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,5 @@
-REATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Check if the user exists before creating it
-SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
-
--- If the user doesn't exist, create it
-IF @user_exists = 0 THEN
-	    CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-END IF;
-
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
+FLUSH PRIVILEGES;
