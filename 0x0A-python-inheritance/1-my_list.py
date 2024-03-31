525be17658ba8b1177a396ce2179c1d43a310a12
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-31 10:40:18 +0000

    new commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class MyList(list):
+    """CLass MyList with the inheritance"""
+
+    def print_sorted(self):
+        """ prints a list in a sorted manner"""
+
+        print(sorted(my_list))
