f968c4fa2addc0a134dfe8e9af2872074423762f
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-31 15:23:39 +0000

    new commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This is a module that has the fuction kind_of
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks whether an object is of a certain class"""
+    return (isinstance(obj, a_class))
