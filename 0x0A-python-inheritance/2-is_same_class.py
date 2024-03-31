5e222611ae9dd0e723af62f054f855c328b6cf31
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-31 15:16:23 +0000

    new commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Module for same class"""
+"""
+Module for same class
+"""
 
 
 def is_same_class(obj, a_class):
-    """checks if object is from a class
-    Args:
-        obj: the object to check
-        a_class: the class to check
-    """
+    """checks if object is from a class"""
     return bool(type(obj) == a_class)
