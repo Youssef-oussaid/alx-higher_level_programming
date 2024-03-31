9bcde81ba716e93f75ef76ad10bfc1dda84a3b5d
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-03-31 15:32:09 +0000

    new commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ this module has the inherits from method
 
 def inherits_from(obj, a_class):
     """checks is an object is of class or a drived/inherited class"""
-    return (isinstance(obj, a_class))
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
