db6783b8f959908252fc6a8463f5b687f7d59dda
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-03 11:45:18 +0000

    a

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module is of an empty class
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if value is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.name = name
+        self.value = value
