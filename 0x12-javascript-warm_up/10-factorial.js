2e4c47d0991a9f4f20fcf1ad0e21494de7ff671f
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-09 03:08:51 +0000

    new commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-
-function computeFactorial(n) {
-    if (isNaN(n) || n < 0) {
-      return 1;
-    } else if (n === 0 || n === 1) {
-      return 1;
-      return n * computeFactorial(n - 1); 
-    }
+const n = process.argv[2];
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
+    return (1);
+  } else {
+    return (n * factorial(n - 1));
   }
-  
-  const input = parseInt(process.argv[2]);
-  console.log(computeFactorial(input));
-  
+}
+console.log(factorial(parseInt(n)));
