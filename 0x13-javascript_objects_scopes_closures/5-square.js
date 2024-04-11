e79577c420af0ca7b2ca26e11cff32868f4a7512
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-11 15:46:01 +0000

    a

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    this.size = size;
+    super(w, h);
+  } 
+}
+module.exports = Square;
