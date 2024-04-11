2a6f15f1381c2c58ca48e70ea9070ee1c795d0e6
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-11 17:27:25 +0000

    a

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,11 +2,6 @@
 let logMeCounter = 0;
 
 exports.logMe = function (item) {
-<<<<<<< HEAD
-  console.log(logMe + ': ' + item);
-  logMe += 1;
-=======
   console.log(logMeCounter + ': ' + item);
-  logMeCounter++;
->>>>>>> 298d27d1cccceec1b7df224e39ab30dd11502367
+  logMeCounter += 1;
 };
