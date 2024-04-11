1ee77f4957ae1dd6c5a12b87bce45b1c57d6fecb
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-11 16:39:00 +0000

    new commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  let size = list.length;
+  const size = list.length;
   for (let i = 0; size > i; i++) {
     if (searchElement === list[i]) {
       count++;
     }
   }
   return count;
-}
+};
