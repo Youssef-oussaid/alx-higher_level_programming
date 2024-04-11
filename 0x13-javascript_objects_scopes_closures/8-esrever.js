df9734f6c8bdba49329d402ee2ca15a6e9b3bfcd
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-11 16:58:13 +0000

    a

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const esrever = [];
-  for (let i = (list.length - 1); i >=0; i--) {
+  for (let i = (list.length - 1); i >= 0; i--) {
     esrever.push(list[i]);
   }
   return esrever;
