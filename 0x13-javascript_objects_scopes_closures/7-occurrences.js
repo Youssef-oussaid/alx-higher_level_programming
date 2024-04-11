b65f84ba4082c12b4a72798c709f00f1fe6bd48e
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-11 16:37:26 +0000

    a

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  let size = list.length();
+  let size = list.length;
   for (let i = 0; size > i; i++) {
     if (searchElement === list[i]) {
       count++;
