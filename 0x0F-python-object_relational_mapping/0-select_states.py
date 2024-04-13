ed9b8ce351632eb4cada5e26d696a82b6ca81d0e
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-13 01:06:47 +0000

    a

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 
 db = MySQLdb.connect(host="localhost",
                      user=sys.argv[1],
-                     password=sys.argv[2],
+                     passwd=sys.argv[2],
                      db=sys.argv[3],
                      port=3306)
 cur = db.cursor()
