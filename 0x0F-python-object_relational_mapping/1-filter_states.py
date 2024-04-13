d9ce6b50f2a83f20ad60f304619b01e8d642007b
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-13 12:44:17 +0000

    a

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists state names"""
+
+
+from sys import argv
+from tkinter.tix import Select
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.Connect(host="localhost", user=argv[1], passwd=argv[2],
+                        db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
