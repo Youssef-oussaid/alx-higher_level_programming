5061adf05b3e9d810d4f7b604d7c7efa149ed40d
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-14 15:54:50 +0100

    a

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,11 +9,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """class represents the states table"""
     __tablename__ = 'States'
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
-
-engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
-Base.metadata.create_all(engine)
