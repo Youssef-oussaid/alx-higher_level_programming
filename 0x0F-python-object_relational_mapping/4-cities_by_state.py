69425a6281942fb1ec1a799de0f555a5c90def05
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-14 14:32:18 +0100

    a

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = state.id "
+                "JOIN states ON cities.state_id = states.id "
                 "ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
