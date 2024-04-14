e0b1a16ced8ff95d562257ee8636508a98ad32cd
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-14 14:05:25 +0100

    a

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Lists cities"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.curser()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.close()
+    db.close()
