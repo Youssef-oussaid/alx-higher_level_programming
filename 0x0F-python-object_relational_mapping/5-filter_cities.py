e95c5b24a5578257a1797546a69794cae540f03b
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-14 15:06:25 +0100

    a

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("""
-               SELECT cities.name FROM cities
-               JOIN states ON cities.state_id = states.id
-               WHERE states.name = %s
+               SELECT cities.name FROM cities 
+               JOIN states ON cities.state_id = states.id 
+               WHERE states.name = %s 
                ORDER cities.id ASC
                """, (argv[4], ))
     print(", ".join(map(lambda x: x[0], cur.fetchall())))
