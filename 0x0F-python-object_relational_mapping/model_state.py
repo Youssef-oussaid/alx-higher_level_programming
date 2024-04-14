1d1453e7d239e66838cb1283c8379a3f345d6dc3
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-14 15:57:29 +0100

    a

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """class state def"""
 
 from sys import argv
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -14,3 +14,7 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False, unique=True)
     name = Column(String(128), nullable=False)
+
+
+engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+Base.metadata.create_all(engine)
