209c7e98a916e04d12788324b9724963647599d3
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-15 19:45:13 +0100

    a

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state_to_update = session.query(State).filter(State.id == 2).all()
+    state_to_update = session.query(State).filter_by(id='2').all()
     if state_to_update:
         state_to_update[0].name = 'New Mexico'
     session.commit()
