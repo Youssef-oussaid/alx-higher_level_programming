cec8396f85abad6e579b7ea5db14308ea3b385a5
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-15 19:50:35 +0100

    a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists states that"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+        , pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).\
+            filter(State.name.like('%a%')).order_by(State.id).all():
+        session.delete(state)
+    session.close()
