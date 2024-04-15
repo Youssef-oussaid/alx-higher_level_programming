d5975902a141f834ca887b00d44211b0b2406de8
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-15 19:38:29 +0100

    a

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""insets a new state"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_to_update = session.query(State).filter_by(id=2).first()
+    state_to_update.name = 'New Mexico'
+    session.commit()
+    session.close()
