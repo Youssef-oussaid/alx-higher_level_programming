f90acc523e76477939f12db47ed5e2b4703134fd
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-29 16:16:17 +0100

    new commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send Post Request & Display Body
-curl -sX POST -H "email: test@gmail.com" "subject: I will always be here for PLD" "$1"
+curl -sX POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
