8f59372708d6951fa5111c48d58b5a6a2ab27ca1
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-29 18:27:26 +0100

    new commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches the status of a website"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+        content_bytes = response.read()
+        content_str = content_bytes.decode('utf-8')
+    
+    print("\t- type:", type(content_bytes))
+    print("\t- content:", content_bytes)
+    print("\t- utf8 content:", content_str)
