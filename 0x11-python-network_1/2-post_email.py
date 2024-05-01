7cec79621371837b3a129bbf42856f40a7689ba4
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-02 00:12:20 +0100

    new commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    email = argv[2]
     data = urllib.parse.urlencode({'email': argv[2]}).encode('utf-8')
     req = urllib.request.Request(argv[1], data=data, method='POST')
     with urllib.request.urlopen(argv[1]) as f:
-        print('Your email is: ',f.read().decode('utf-8'))
+        print(f"Your email is: {email}")
