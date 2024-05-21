6a9339a1f6b03af8c40174354ca517b7320c2099
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-22 00:03:53 +0100

    new commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,8 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (err, response) => {
+  if (err) {
+    console.err(err);
+  }
   console.log('code:', response.statusCode);
 });
