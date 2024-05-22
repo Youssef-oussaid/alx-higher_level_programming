8729d458386517b81dd8f7de9f496d8ed1ea63a5
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-22 10:43:32 +0100

    new commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,14 +7,6 @@ request(url, (error, response, body) => {
     console.error(error);
     return;
   }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    return;
-  }
-  try {
     const data = JSON.parse(body);
     console.log(data.title);
-  } catch (e) {
-    console.error('Error parsing JSON:', e);
-  }
 });
