212794e754cd81259b5426ddc4c359f0a4cd1290
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-22 21:47:32 +0100

    new commit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
-request(url, (error,response, body) => {
-    if (error){
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+    if (error) {
         console.error(error);
         return;
     }
-    const data = JSON.parse(body)
-    let counter = data.films.length;
+    const data = JSON.parse(body);
+    let counter = 0;
+
+    data.results.forEach(result => {
+        result.characters.forEach(character => {
+            if (character.includes('/18/')) {
+                counter++;
+            }
+        });
+    });
     console.log(counter);
 });
