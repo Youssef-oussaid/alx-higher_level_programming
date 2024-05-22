c9e2c68ef5ecfa2c5f5915663a78c71232edaf42
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-22 22:17:11 +0100

    new commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -10,23 +10,21 @@ request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   }
-  
-  const contentType = response.headers['content-type'];
-  
-  if (contentType && contentType.includes('application/json')) {
 
-      const data = JSON.parse(body);
-      fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8', (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
- 
+  const contentType = response.headers['content-type'];
+
+  if (contentType && contentType.includes('application/json')) {
+    const data = JSON.parse(body);
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   } else {
     fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
-      } 
+      }
     });
   }
 });
